399e95f98689c4df01ab8645b4571ee0df6abee5
Jonathan <jonathan_maccas@outlook.com>
2024-05-18 10:33:37 -0300

    fix: formatar o código

--- modulo-iniciante/numeros.py
+++ modulo-iniciante/numeros.py
@@ -1,37 +1,37 @@
 # Estão os três tipos de dados para trabalhar com números dentro da linguagem
 NUMBER_INTEGER = 10
-NUMBER_FLOAT=25.97
-NUMBER_COMPLEX= complex(1,-2)
+NUMBER_FLOAT = 25.97
+NUMBER_COMPLEX = complex(1, -2)
 
 # OPERAÇÕES
-RESULT_INT_ADD= NUMBER_INTEGER - 2
-RESULT_INT_SUB= NUMBER_INTEGER + 2
-RESULT_INT_DIV= NUMBER_INTEGER / 2
-RESULT_INT_MULT= NUMBER_INTEGER * 2
-RESULT_INT_FRAC= NUMBER_INTEGER // 8
-RESULT_INT_REST= NUMBER_INTEGER % 4
-RESULT_INT_POTE= NUMBER_INTEGER ** 5
-RESULT_FLOAT= -NUMBER_FLOAT
+RESULT_INT_ADD = NUMBER_INTEGER - 2
+RESULT_INT_SUB = NUMBER_INTEGER + 2
+RESULT_INT_DIV = NUMBER_INTEGER / 2
+RESULT_INT_MULT = NUMBER_INTEGER * 2
+RESULT_INT_FRAC = NUMBER_INTEGER // 8
+RESULT_INT_REST = NUMBER_INTEGER % 4
+RESULT_INT_POTE = NUMBER_INTEGER**5
+RESULT_FLOAT = -NUMBER_FLOAT
 print(RESULT_INT_ADD)
-print('-----------------------------------------------')
+print("-----------------------------------------------")
 
 print(RESULT_INT_SUB)
-print('-----------------------------------------------')
+print("-----------------------------------------------")
 
 print(RESULT_INT_DIV)
-print('-----------------------------------------------')
+print("-----------------------------------------------")
 
 print(RESULT_INT_MULT)
-print('-----------------------------------------------')
+print("-----------------------------------------------")
 
 print(RESULT_INT_FRAC)
-print('-----------------------------------------------')
+print("-----------------------------------------------")
 
 print(RESULT_INT_REST)
-print('-----------------------------------------------')
+print("-----------------------------------------------")
 
 print(RESULT_INT_POTE)
-print('-----------------------------------------------')
+print("-----------------------------------------------")
 
 print(RESULT_FLOAT)
-print('-----------------------------------------------')
+print("-----------------------------------------------")
--- modulo-iniciante/strings.py
+++ modulo-iniciante/strings.py
@@ -1,41 +1,43 @@
-print('Ola')
+print("Ola")
 print("ola")
-print("""
+print(
+    """
     Olá
       Mundo
       Novo
-""")
-print('-----------------------------------------------------------------------------')
+"""
+)
+print("-----------------------------------------------------------------------------")
 # Tamanho da string e pegar uma parte dela
 NOME = "Jonathan"
 print(len(NOME))
 print(NOME[3])
 print(NOME[2:-1])
 
-print('-----------------------------------------------------------------------------')
+print("-----------------------------------------------------------------------------")
 # OPERAÇÕES
 
-print('Operação in')
-print(NOME in 'Jonathan') # True
-print(NOME in 'Jo') # False
+print("Operação in")
+print(NOME in "Jonathan")  # True
+print(NOME in "Jo")  # False
 
-print('Operação not in')
-print(NOME not in 'Jonathan') # False
-print(NOME not in 'Jo') # True
+print("Operação not in")
+print(NOME not in "Jonathan")  # False
+print(NOME not in "Jo")  # True
 
-print('Operação concatenação')
-print(NOME + ' ' + 'ão')
+print("Operação concatenação")
+print(NOME + " " + "ão")
 
-print('Operação de repição')
-print(NOME*3)
+print("Operação de repição")
+print(NOME * 3)
 
-print('----------------------------------------------------------------------------')
+print("----------------------------------------------------------------------------")
 
 # MÉTODOS
 print(NOME.capitalize())
-print(NOME.count('a'))
-print(NOME.startswith('n'))
-print(NOME.endswith('J'))
-print("Jonathan está em casa dormindo".split(' '))
-print(' '.join(['Jonathan', 'em casa']))
-print(NOME.replace('Jonathan', "João"))
+print(NOME.count("a"))
+print(NOME.startswith("n"))
+print(NOME.endswith("J"))
+print("Jonathan está em casa dormindo".split(" "))
+print(" ".join(["Jonathan", "em casa"]))
+print(NOME.replace("Jonathan", "João"))
