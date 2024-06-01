a4d4846800aa31e6eee594982c91551cd09d3e38
Jonathan <jonathan_maccas@outlook.com>
2024-06-01 16:16:37 -0300

    feat: adicionar métodos de classe

--- modulo-intermediario/poo.py
+++ modulo-intermediario/poo.py
@@ -10,3 +10,26 @@ class Pessoa:
 
 p1 = Pessoa("João", 21)
 p1.falar("Maria")
+
+
+# Classe com métodos de classe
+class Carro:
+    ano_lancamento = 2018
+
+    def __init__(self, marca, valor):
+        self.marca = marca
+        self.valor = valor
+
+    def carro_velho(self):
+        if self.valor <= 10.000:
+            print("Carro antigo com o valor abaixo de 10.000")
+            return
+        print(f"O carro da marca {self.marca} é novo")
+
+    @classmethod
+    def novo_carro(cls, marca):
+        return cls(marca, 25.520)
+
+
+c1 = Carro.novo_carro("Ford")
+print(c1.valor)
