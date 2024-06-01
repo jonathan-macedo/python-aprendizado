4bb7e57af02269852982198c48dddaf6072f1cd6
Jonathan <jonathan_maccas@outlook.com>
2024-06-01 16:20:58 -0300

    feat: adicionar conceitos de getters e setters

--- modulo-intermediario/poo.py
+++ modulo-intermediario/poo.py
@@ -61,3 +61,33 @@ class Cliente:
 
 
 print(Cliente.gera_id())
+
+
+# Gatters e Setters
+class Produto:
+    def __init__(self, nome, preco):
+        self.nome = nome
+        self.preco = preco
+
+    def desconto(self, percentual):
+        self.preco = self.preco - (self.preco * (percentual / 100))
+
+    # Getter
+    @property
+    def preco(self):
+        return self._preco
+
+    # Setter
+    @preco.setter
+    def preco(self, valor):
+        if isinstance(valor, str):
+            valor = float(valor.replace("R$", ""))
+        self._preco = valor
+
+
+p1 = Produto("Camiseta", 50)
+p1.desconto(10)
+print(p1.preco)
+p2 = Produto("Calça", "R$12")
+p1.desconto(20)
+print(p2.preco)
