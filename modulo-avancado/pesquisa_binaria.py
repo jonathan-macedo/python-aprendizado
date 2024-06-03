ce11a7118271e349ed2e141acd9da877efb646cc
Jonathan <jonathan_maccas@outlook.com>
2024-06-03 11:41:25 -0300

    fix: corrigir identação e tirar a tipagem das variáveis

--- modulo-avancado/pesquisa_binaria.py
+++ modulo-avancado/pesquisa_binaria.py
@@ -1,19 +1,20 @@
-def pesquisa_binaria(lista: list, item: int):
+def pesquisa_binaria(lista, item):
     baixo = 0
     alto = len(lista) - 1
 
     while baixo <= alto:
-        meio = (baixo + alto) / 2
+        meio = (baixo + alto) // 2
         chute = lista[meio]
         if chute == item:
             return meio
-        
+
         if chute > item:
             alto = meio - 1
         else:
             baixo = meio + 1
-        
-        return None
+
+    return None
+
 
 minha_lista = [1, 3, 5, 7, 9]
 
