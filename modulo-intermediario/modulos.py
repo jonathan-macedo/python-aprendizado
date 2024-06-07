f03fefd859cdecf834dd1d74b0e93f6ef233d6c9
Jonathan <jonathan_maccas@outlook.com>
2024-06-07 09:02:35 -0300

    feat: adicionar modulos de importação

---
+++ modulo-intermediario/modulos.py
@@ -0,0 +1,12 @@
+# Importa todas as funções desse modulo
+# import math
+
+# Importa, do pacote Math, somente sqrt
+from math import sqrt
+
+numero1 = int(input("Informe um número para uma conta: "))
+
+# resultado = int(math.sqrt(numero1))
+resultado = int(sqrt(numero1))
+
+print(resultado)
